b6e9bc917fdfdd7395cd56e7a60fa764afb46b57
elmirmammadli1995 <149464393+elmirmammadli1995@users.noreply.github.com>
2025-03-28 18:44:03 +0000

    CorsUrl

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from .config import API_KEY
 app = Flask(__name__)
 
 CORS(app, resources = {r"/api/news": {
-    "origins":  [],
+    "origins":  ["https://elmirmammadli1995.github.io"],
     "methods": ["GET"] 
 }})
 
